c92457b649e10983099ae4512d685c176c64ac43
Nicolás Quiroz <nh.quiroz@gmail.com>
2015-07-17 13:39:37 -0300

    Add exercise 11

---
+++ ex11.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+print "How old are you? (years):",
+age    = raw_input()
+print "How tall are you? (centimetres):",
+height = raw_input()
+print "How much do you weigh? (kilograms):",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
