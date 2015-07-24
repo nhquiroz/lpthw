391f0663ba86d3e57ab9d6c201501cfdd5b02372
Nicolás Quiroz <nh.quiroz@gmail.com>
2015-07-24 16:18:17 -0300

    Edit ex15 code to close the files

--- ex15.py
+++ ex15.py
@@ -5,9 +5,11 @@ text_file = open(filename)
 
 print "Here's your file %r:" % filename
 print text_file.read()
+text_file.close()
 
 print "Type the filename again:"
 filename_again = raw_input("> ")
 
 text_file_again = open(filename_again)
 print text_file_again.read()
+text_file_again.close()
