eb1020cbb7b5e765c12d9711f80daaa56a1b8008
Nicolás Quiroz <nh.quiroz@gmail.com>
2015-07-27 02:44:10 -0300

    Update ex05.py

--- ex05.py
+++ ex05.py
@@ -5,15 +5,12 @@ my_weight = 72
 my_eyes   = "green"
 my_hair   = "brown"
 
-print "Let's talk about %s." % (my_name)
-print "He's %d centimetres tall." % (my_height)
-print "He's %d years old." % (my_age)
-print "He weights %d kilos." % (my_weight)
-print "His eyes are %s and his hair %s" % (my_eyes, my_hair)
+print("Let's talk about %s.")            % (my_name)
+print("He's %d centimetres tall.")       % (my_height)
+print("He's %d years old.")              % (my_age)
+print("He weights %d kilos.")            % (my_weight)
+print("His eyes are %s and his hair %s") % (my_eyes, my_hair)
 
 # the next line is a little more tricky
-print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
-print "Bye!"
-
-
-
+print("If I add %d, %d, and %d I get %d.") % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+print("Bye!")
