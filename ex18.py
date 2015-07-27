789f4cde7c2b88a9a5c4abef0d5dcb650ddb471d
Nicolás Quiroz <nh.quiroz@gmail.com>
2015-07-27 04:51:32 -0300

    Update ex18.py to follow PEP8

--- ex18.py
+++ ex18.py
@@ -2,11 +2,14 @@ def print_two_arguments(*args):
     arg1, arg2 = args   # unpacking arguments to variables
     print("arg1: %r, arg2: %r") % (arg1, arg2)
 
+
 def print_two_arguments_again(arg1, arg2):   # better than the previous version
     print("arg1: %r, arg2: %r") % (arg1, arg2)
 
+
 def print_one_argument(arg):
     print("arg: %r") % arg
+
 
 def print_without_arguments():
     print("I got nothin'.")
