01f596c780f18216983034bb230c0a1e70299ae9
Nicolás Quiroz <nh.quiroz@gmail.com>
2015-07-27 19:12:00 -0300

    Add ex20.py and ex20_sample.txt

--- ex20.py
+++ ex20.py
@@ -7,11 +7,36 @@ from sys import argv
 
 # <--- beginning of the script --->
 
-script, input_file = argv
+input_file = argv[1]
 
 
-def print_all(f):
-    print f.read()
+def print_all(file):
+    print file.read()
 
 
-def rewind()
+def rewind(file):
+    file.seek(0)
+
+
+def print_a_line(line_count, file):
+    print line_count, file.readline()
+
+
+current_file = open(input_file)
+
+print("Fist let's print the whole file:\n")
+print_all(current_file)
+
+print("Now let's rewind, kind of like a tape.")
+rewind(current_file)
+
+print("Let's print three lines:")
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
