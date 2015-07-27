647af78431ee9dafd03e91fc094c58b5106ee6f7
Nicolás Quiroz <nh.quiroz@gmail.com>
2015-07-27 00:26:48 -0300

    Add ex17 with sample text files

---
+++ ex17.py
@@ -0,0 +1,28 @@
+# <--- modules needed --->
+
+from sys import argv
+from os.path import exists
+
+
+# <--- beginning of the script --->
+
+source_file, target_file = argv[1], argv[2]
+
+print "Copying from %r to %r." % (source_file, target_file)
+
+input_file = open(source_file)
+input_data = input_file.read()
+
+print "The input file is %d bytes long." % len(input_data)
+
+print "Does the output file exist? %r" % exists(target_file)
+print "Ready. Hit RETURN to continue or CTRL-C to abort."
+raw_input('> ')
+
+output_file = open(target_file, 'w')
+output_file.write(input_data)
+
+print "Alright, all done."
+
+output_file.close()
+input_file.close()
