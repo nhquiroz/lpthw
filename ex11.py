5324bfc064a46e287c7c24926332dc142e6f8851
Nicolás Quiroz <nh.quiroz@gmail.com>
2015-07-27 02:45:21 -0300

    Update ex11.py

--- ex11.py
+++ ex11.py
@@ -1,8 +1,8 @@
-print "How old are you? (years):",
+print("How old are you? (years):"),
 age    = raw_input()
-print "How tall are you? (centimetres):",
+print("How tall are you? (centimetres):"),
 height = raw_input()
-print "How much do you weigh? (kilograms):",
+print("How much do you weigh? (kilograms):"),
 weight = raw_input()
 
-print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+print("So, you're %r old, %r tall and %r heavy.") % (age, height, weight)
