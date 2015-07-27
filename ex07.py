50b17ab64eaaf7822bf708fa745342313db51ea0
Nicolás Quiroz <nh.quiroz@gmail.com>
2015-07-27 02:44:34 -0300

    Update ex07.py

--- ex07.py
+++ ex07.py
@@ -1,10 +1,10 @@
-print "Mary had a little lamb."
-print "Its fleece was white as %s." % "snow"
-print "And everywhere that Mary went."
-print '.' * 10 # what'd that do?
+print("Mary had a little lamb.")
+print("Its fleece was white as %s." % "snow")
+print("And everywhere that Mary went.")
+print('.') * 10
 
 for i in xrange(10):
-    print '.'
+    print('.')
 
 end1  = 'C'
 end2  = 'h'
@@ -19,6 +19,5 @@ end10 = 'g'
 end11 = 'e'
 end12 = 'r'
 
-# watch that comma at the end. Try removing it to see what happens
-print end1 + end2 + end3 + end4 + end5 + end6,
-print end7 + end8 + end9 + end10 + end11 + end12
+print(end1 + end2 + end3 + end4 + end5 + end6),
+print(end7 + end8 + end9 + end10 + end11 + end12)
