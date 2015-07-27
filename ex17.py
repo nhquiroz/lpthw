89120a2911e0d8284f04d45557188248cc75816d
Nicolás Quiroz <nh.quiroz@gmail.com>
2015-07-27 02:46:23 -0300

    Update ex17.py

--- ex17.py
+++ ex17.py
@@ -8,18 +8,18 @@ from os.path import exists
 
 source_file, target_file = argv[1], argv[2]
 
-print "Copying from %r to %r." % (source_file, target_file)
+print("Copying from %r to %r.") % (source_file, target_file)
 
 input_file = open(source_file).read()
 
-print "The input file is %d bytes long." % len(input_file)
+print("The input file is %d bytes long.") % len(input_file)
 
-print "Does the output file exist? %r" % exists(target_file)
-print "Ready. Hit RETURN to continue or CTRL-C to abort."
+print("Does the output file exist? %r") % exists(target_file)
+print("Ready. Hit RETURN to continue or CTRL-C to abort.")
 raw_input('> ')
 
 output_file = open(target_file, 'w')
 output_file.write(input_file)
 
-print "Alright, all done."
+print("Alright, all done.")
 output_file.close()
