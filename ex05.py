a5a7b1f631afed88b229133190483fbf765a5a15
Nicolás Quiroz <nh.quiroz@gmail.com>
2015-07-27 15:09:18 -0300

    Remove assignment spaces from ex05.py to stick with PEP8, E221

--- ex05.py
+++ ex05.py
@@ -1,14 +1,14 @@
-my_name   = "Nico"
-my_age    = 23
+my_name = "Nico"
+my_age = 23
 my_height = 184
 my_weight = 72
-my_eyes   = "green"
-my_hair   = "brown"
+my_eyes = "green"
+my_hair = "brown"
 
-print("Let's talk about %s.")            % (my_name)
-print("He's %d centimetres tall.")       % (my_height)
-print("He's %d years old.")              % (my_age)
-print("He weights %d kilos.")            % (my_weight)
+print("Let's talk about %s.") % (my_name)
+print("He's %d centimetres tall.") % (my_height)
+print("He's %d years old.") % (my_age)
+print("He weights %d kilos.") % (my_weight)
 print("His eyes are %s and his hair %s") % (my_eyes, my_hair)
 
 print("If I add %d, %d, and %d I get %d.") % (my_age, my_height,
