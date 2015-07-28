c59c49b42b532ca70d042e9e170d530ea04f0cea
Nicolás Quiroz <nh.quiroz@gmail.com>
2015-07-28 03:47:42 -0300

    Change the way of opening files, use with instead

--- ex17.py
+++ ex17.py
@@ -12,16 +12,16 @@ source_file, target_file = argv[1], argv[2]
 
 print("Copying from %r to %r.") % (source_file, target_file)
 
-input_file = open(source_file).read()
-
-print("The input file is %d bytes long.") % len(input_file)
+with open(source_file) as input_file:
+    content = input_file.read()
+    print("The input file is %d bytes long.") % len(content)
 
 print("Does the output file exist? %r") % exists(target_file)
 print("Ready. Hit RETURN to continue or CTRL-C to abort.")
 raw_input('> ')
 
-output_file = open(target_file, 'w')
-output_file.write(input_file)
+with open(target_file, 'w') as output_file:
+    content = output_file.read()
+    content.write(input_file)
 
 print("Alright, all done.")
-output_file.close()
