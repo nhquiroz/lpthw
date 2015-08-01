4e83c932ef9ef21907203fbd1e63f59bcb1a1806
Nicolás Quiroz <nh.quiroz@gmail.com>
2015-07-31 21:08:54 -0300

    Update ex20

--- ex20.py
+++ ex20.py
@@ -26,16 +26,15 @@ with open(input_file) as current_file:
     print("Fist let's print the whole file:\n")
     print_all(current_file)
 
-print("Now let's rewind, kind of like a tape.")
-rewind(current_file)
+    print("Now let's rewind, kind of like a tape.")
+    rewind(current_file)
 
-print("Let's print three lines:")
+    print("Let's print three lines:")
+    current_line = 1
+    print_a_line(current_line, current_file)
 
-current_line = 1
-print_a_line(current_line, current_file)
+    current_line = current_line + 1
+    print_a_line(current_line, current_file)
 
-current_line = current_line + 1
-print_a_line(current_line, current_file)
-
-current_line = current_line + 1
-print_a_line(current_line, current_file)
+    current_line = current_line + 1
+    print_a_line(current_line, current_file)
