550b9e595ea7e89b926f6a8b04ec12b91450003b
Nicolás Quiroz <nquiroz@dc.uba.ar>
2015-09-25 01:31:55 -0300

    Update ex33

--- ex33.py
+++ ex33.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python
 
+a_number = 0
+a_list_of_numbers = []
+
+while a_number < 6:
+    print("At the top i is {:d}".format(a_number))
+    a_list_of_numbers.append(a_number)
+
+    a_number += 1
+    print("Numbers now: ", a_list_of_numbers)
+    print("At the bottom a_number is {:d}".format(a_number))
+
+print("The numbers: ")
+
+for number in a_list_of_numbers:
+    print(number)
